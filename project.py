a76414c1612360094f56283f12160ff62b740341
Luiz Felipe Rodrigues <lfeliiipe@hotmail.com>
2025-02-10 18:35:07 -0300

    Add option to remove products from the persisted list

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import re
 import sys
 from persist import Persist
 
@@ -9,22 +10,21 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Monitor product prices")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-a", "--add", action="extend", nargs="+", help="Add one or more products", type=str)
-    group.add_argument("-f", "--fetch", action="store_true", help="Fetch prices for listed products")
+    group.add_argument("-r", "--remove", action="store_true", help="Remove product(s)")
     group.add_argument("-l", "--list", action="store_true", help="List products")
     args = parser.parse_args()
 
     p: Persist = Persist(".persisted/products_list.csv")
 
-    # Menu
+    # Add new products
     if args.add:
+
         products: list = add_products(args.add, p)
         print("Updated Products List:")
         for product in products:
             print(product)
 
-    elif args.fetch:
-        fetch()
-
+    # List persisted products
     elif args.list:
 
         products: list = list_products(p)
@@ -34,6 +34,27 @@ def main() -> None:
 
         for product in products:
             print(product)
+
+    # Remove products from persisted list
+    elif args.remove:
+        
+        print("Insert the products' index(es) to be removed")
+        print("Example: 2 3 4 12")
+
+        products: list = list_products(p)
+        enum_products: enumerate = enumerate(products, start=1)
+        for i, product in enum_products:
+            print(f"Index: {i}, Product: {product}")
+
+        if to_remove := get_ints(input("\nType here: ")):
+            updated_products: list = remove_products(to_remove, products, p)
+            print("\nUpdated products:")
+            for product in updated_products:
+                print(product)
+            
+            return
+        
+        print("No matches")
 
 
 def add_products(new_products: list, p: Persist) -> list:
@@ -46,8 +67,10 @@ def add_products(new_products: list, p: Persist) -> list:
     return list_products(p)
 
 
-def fetch():
-    ...
+def get_ints(prompt: str) -> list:
+
+    if matches := re.findall(r"\b(\d+)\b", prompt.strip()):
+        return [ int(match) for match in matches ]
 
 
 def list_products(p: Persist) -> list:
@@ -57,6 +80,17 @@ def list_products(p: Persist) -> list:
     file.close()
 
     return products
+
+
+def remove_products(remove_list: list, products: list, p: Persist) -> list:
+    
+    file = p.open_to("w")
+    updated_products: list = [ product for i, product in enumerate(products, start=1) if i not in remove_list ]
+    for product in updated_products:
+        file.write(f"{product}\n")
+    file.close()
+    
+    return list_products(p)
 
 
 if __name__ == "__main__":
